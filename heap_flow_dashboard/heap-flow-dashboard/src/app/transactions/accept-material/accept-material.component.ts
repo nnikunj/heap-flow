8be4a952a2cfa41b7b73d3ecc263c971625ea7a3
Prakriti <prakriti.patra@gmail.com>
2020-05-09 17:15:53 +0530

    Adding Form to transactions

--- heap_flow_dashboard/heap-flow-dashboard/src/app/transactions/accept-material/accept-material.component.ts
+++ heap_flow_dashboard/heap-flow-dashboard/src/app/transactions/accept-material/accept-material.component.ts
@@ -10,10 +10,39 @@ export class AcceptMaterialComponent implements OnInit {
 
 
   acceptMaterialModel :AcceptMaterial;
-  
-  constructor() { }
+  todayDate = new Date().toString;
+
+  constructor(AcceptMaterial) {
+    this.acceptMaterialModel = new AcceptMaterial();
+    // this.acceptMaterialModel.vendor = data.vendor;
+    this.acceptMaterialModel.recordDate = "";
+    this.acceptMaterialModel.grn = "";
+    this.acceptMaterialModel.invoice = "";
+
+    this.acceptMaterialModel.quantity = 0;
+    this.acceptMaterialModel.price = 0.00;
+    this.acceptMaterialModel.classification = "";
+    this.acceptMaterialModel.acceptingMaterialCode = "";
+
+  }
 
   ngOnInit(): void {
+
   }
+
+  onClickSubmit(data) {
+    console.log(data);
+    this.acceptMaterialModel.vendor = data.vendor;
+    this.acceptMaterialModel.recordDate = data.recordDate;
+    this.acceptMaterialModel.grn = data.grn;
+    this.acceptMaterialModel.invoice = data.invoice;
+
+    this.acceptMaterialModel.quantity = data.quantity;
+    this.acceptMaterialModel.price = data.price;
+    this.acceptMaterialModel.classification = data.classification;
+    this.acceptMaterialModel.acceptingMaterialCode = data.acceptingMaterialCode;
+
+    console.log(this.acceptMaterialModel);
+ }
 
 }
