9b5d134363d82666ca284f635c06c817b3eb5721
Nilabh Nikunj <nilabhnikunj@gmail.com>
2020-05-17 13:37:10 +0530

    Update make-dist.sh

--- make-dist.sh
+++ make-dist.sh
@@ -8,5 +8,5 @@ cp db-container/target/heap-flow-db.tar.gz dist/heap-flow/heap-flow-db.tar.gz
 cp heap_flow/target/heap-flow-app.tar.gz dist/heap-flow/heap-flow-app.tar.gz 
 cp heap_flow_dashboard/heap-flow-dashboard/dist/heap-flow-dashboard.tar.gz  dist/heap-flow/heap-flow-dashboard.tar.gz
 
-zip heap-flow.zip dist/heap-flow
+zip heap-flow.zip dist/heap-flow/*
 echo 'created zip'
