e1eac21b3ca45bd72ec74dfded612e333a790287
Nilabh Nikunj <nikunjnilabh@gmail.com>
2020-06-08 08:09:05 +0530

    All scripts for packaging

---
+++ dist/heap-flow/db_schema_changes.sql
@@ -0,0 +1,5 @@
+alter table egress_ledgers add column issue_slip_number varchar(255), add column outgoing_mat_price double;
+
+alter table egress_ledgers MODIFY COLUMN record_date DATETIME;
+  
+alter table ingress_ledgers MODIFY COLUMN po_date DATETIME, MODIFY COLUMN invoice_date DATETIME, MODIFY COLUMN record_date DATETIME;
--- make-dist-without-db.sh
+++ make-dist-without-db.sh
@@ -8,7 +8,7 @@ rm -rf dist/heap-flow/heap-flow-dashboard.tar.gz
 echo removing old dist/heap-flow/heap-flow-rpt.tar.gz
 rm -rf dist/heap-flow/heap-flow-rpt.tar.gz
 
-cp db-container/target/heap-flow-db.tar.gz dist/heap-flow/heap-flow-db.tar.gz 
+#cp db-container/target/heap-flow-db.tar.gz dist/heap-flow/heap-flow-db.tar.gz 
 cp heap_flow/target/heap-flow-app.tar.gz dist/heap-flow/heap-flow-app.tar.gz 
 cp heap_flow_dashboard/heap-flow-dashboard/dist/heap-flow-dashboard.tar.gz  dist/heap-flow/heap-flow-dashboard.tar.gz
 cp heap_flow_reports/heap_flow_reports/target/heap-flow-rpt.tar.gz dist/heap-flow/heap-flow-rpt.tar.gz
