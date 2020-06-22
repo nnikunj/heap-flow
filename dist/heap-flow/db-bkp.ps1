f5f83b2ab541771852fe221c8ee62c7ecdfa6a9c
Nilabh Nikunj <nikunjnilabh@gmail.com>
2020-06-22 07:47:21 +0530

    Dist Dir files

--- dist/heap-flow/db-bkp.ps1
+++ dist/heap-flow/db-bkp.ps1
@@ -1,3 +1,3 @@
 Write-HOST 'Taking DB backup.'
-docker exec heap-db /usr/bin/mysqldump -u root --password=\!HK06IS033  heapdb > db-backup$(get-date -f yyyy-MMM-dd).sql
+docker exec heap-db /usr/bin/mysqldump -u root --password=!HK06IS033 --databases heapdb --result-file=db-backup$(get-date -f yyyy-MMM-dd).sql
 Write-HOST 'DB backup complete.'
