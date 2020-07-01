36df26f8a1971d69d5df57e2e3812973723623b5
Nilabh Nikunj <nikunjnilabh@gmail.com>
2020-07-01 08:31:23 +0530

    Script changes with new container.

--- dist/heap-flow/app-installer.ps1
+++ dist/heap-flow/app-installer.ps1
@@ -1,6 +1,15 @@
 Write-HOST 'Starting deploy of Heap Flow App.'
 Write-HOST $PSScriptRoot
- 
+
+Write-HOST 'Load Docker Proxy Container'
+Write-HOST 'Stopping Proxy'
+docker rm heap-flow-proxy -f
+docker rmi heap-flow-proxy:1.0.0
+Write-HOST 'Removed old Proxy'
+docker load -i $PSScriptRoot\heap-flow-proxy.tar.gz
+
+Write-HOST '$PSScriptRoot\heap-flow-proxy.tar.gz loaded'
+docker images
 Write-HOST 'Load Docker App Container'
 Write-HOST 'Stopping app'
 docker stop heap-flow-app
@@ -11,8 +20,7 @@ Write-HOST 'Removed old app'
 docker load -i $PSScriptRoot\heap-flow-app.tar.gz
 
 Write-HOST '$PSScriptRoot\heap-flow-app.tar.gz loaded'
-docker images 
-
+docker images
 
 
 Write-HOST 'Load Docker Dashboard Container'
@@ -26,10 +34,10 @@ Write-HOST 'Removed dashbaord app'
 docker load -i $PSScriptRoot\heap-flow-dashboard.tar.gz
 
 Write-HOST '$PSScriptRoot\heap-flow-dashboard.tar.gz loaded'
-docker images 
+docker images
 
 Write-HOST 'Load Docker Report Container'
- 
+
 Write-HOST 'Stopping rpt app'
 docker stop heap-flow-rpt
 Write-HOST 'Removing old rpt app'
@@ -40,10 +48,10 @@ Write-HOST 'Removed old rpt app'
 docker load -i $PSScriptRoot\heap-flow-rpt.tar.gz
 
 Write-HOST '$PSScriptRoot\heap-flow-rpt.tar.gzloaded'
-docker images 
+docker images
 
- 
- 
+
+
 Write-HOST 'Starting App container.'
 docker rm heap-flow-app -f
 docker run   --restart=always -d --name heap-flow-app --link heap-db -p 9443:9443 -v $PSScriptRoot\logs\app:/opt/paranika/logs/ -d heap-flow-app:1.0.0
@@ -60,8 +68,13 @@ Start-Sleep -Seconds 5
 
 Write-HOST 'Starting Dashboard container.'
 docker rm dashboard -f
-docker run   --restart=always -d --name dashboard -p 80:8080 -d heap-flow-dashboard:1.0.0
+docker run   --restart=always -d --name dashboard -p 8080:8080 -d heap-flow-dashboard:1.0.0
 Write-HOST 'Started dashboard container.'
+
+Write-HOST 'Starting Proxy container.'
+docker rm heap-flow-proxy -f
+docker run --restart=always --name heap-flow-proxy --link dashboard --link heap-flow-app --link heap-flow-rpt  -p 80:80 -d  heap-flow-proxy:1.0.0
+
 Write-HOST 'Deployment complete, Access APP at http://localhost'
 
 Start-Sleep -Seconds 10
--- make-dist.sh
+++ make-dist.sh
@@ -7,12 +7,16 @@ echo removing old dist/heap-flow/heap-flow-dashboard.tar.gz
 rm -rf dist/heap-flow/heap-flow-dashboard.tar.gz
 echo removing old dist/heap-flow/heap-flow-rpt.tar.gz
 rm -rf dist/heap-flow/heap-flow-rpt.tar.gz
+echo removing old dist/heap-flow/heap-flow-proxy.tar.gz
+rm -rf dist/heap-flow/heap-flow-proxy.tar.gz
 
-cp db-container/target/heap-flow-db.tar.gz dist/heap-flow/heap-flow-db.tar.gz 
-cp heap_flow/target/heap-flow-app.tar.gz dist/heap-flow/heap-flow-app.tar.gz 
+
+cp db-container/target/heap-flow-db.tar.gz dist/heap-flow/heap-flow-db.tar.gz
+cp heap_flow/target/heap-flow-app.tar.gz dist/heap-flow/heap-flow-app.tar.gz
 cp heap_flow_dashboard/heap-flow-dashboard/dist/heap-flow-dashboard.tar.gz  dist/heap-flow/heap-flow-dashboard.tar.gz
 cp heap_flow_reports/heap_flow_reports/target/heap-flow-rpt.tar.gz dist/heap-flow/heap-flow-rpt.tar.gz
 cp db-container/db_scripts/db_schema_changes.sql   dist/heap-flow/db_schema_changes.sql
 cp db-container/db-bkp.ps1 dist/heap-flow/db-bkp.ps1
+cp heap_haproxy/target/heap-flow-proxy.tar.gz dist/heap-flow/heap-flow-proxy.tar.gz
 zip heap-flow.zip dist/heap-flow/*
 echo 'created zip'
